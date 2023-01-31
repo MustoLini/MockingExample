98300c32ea102a8ae3d769b7cdefa206cc1807c7
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-01-31 22:07:11 +0100

    Refactoring Employees.java

--- src/main/java/com/example/Employees.java
+++ src/main/java/com/example/Employees.java
@@ -4,26 +4,26 @@ import java.util.List;
 
 public class Employees {
 
-	private final EmployeeRepository employeeRepository;
-	private final BankService bankService;
+    private final EmployeeRepository employeeRepository;
+    private final BankService bankService;
 
-	public Employees(EmployeeRepository employeeRepository, BankService bankService) {
-		this.employeeRepository = employeeRepository;
-		this.bankService = bankService;
-	}
+    public Employees(EmployeeRepository employeeRepository, BankService bankService) {
+        this.employeeRepository = employeeRepository;
+        this.bankService = bankService;
+    }
 
-	public int payEmployees() {
-		List<Employee> employees = employeeRepository.findAll();
-		int payments = 0;
-		for (Employee employee : employees) {
-			try {
-				bankService.pay(employee.getId(), employee.getSalary());
-				employee.setPaid(true);
-				payments++;
-			} catch (RuntimeException e) {
-				employee.setPaid(false);
-			}
-		}
-		return payments;
-	}
+    public int payEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        int payments = 0;
+        for (Employee employee : employees) {
+            try {
+                bankService.pay(employee.getId(), employee.getSalary());
+                employee.setPaid(true);
+                payments++;
+            } catch (RuntimeException e) {
+                employee.setPaid(false);
+            }
+        }
+        return payments;
+    }
 }
